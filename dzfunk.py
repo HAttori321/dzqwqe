cdb2a28b318756c8d224c271fdd7c6819c7a924d
HAttori321 <berezaoksana1@gmail.com>
2023-12-29 21:40:32 +0200

    first commit

---
+++ dzfunk.py
@@ -0,0 +1,26 @@
+#1
+# def dobytok(list):
+#     result=1
+#     for num in list:
+#         result*=num
+#     return result
+#2
+# def minimum(list):
+#     return min(list)
+#3
+# def list(n):
+#     if n<2:
+#         return False
+#     for i in range(2,int(n**0.5)+1):
+#         if n%i==0:
+#             return False
+#     return True
+# def count(list):
+#     count=0
+#     for num in list:
+#         if list(num):
+#             count+=1
+#     return count
+#4
+# не вспів,дороблю
+
